669962485a89a970ec5660459405677b83db4d55
Sean Dandrea <SeanOfAndrea@outlook.com>
2023-05-24 09:29:31 -0400

    Initial commit

---
+++ BlockPausePlugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using Eremite.Services;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ATSBlockPause
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class BlockPausePlugin : BaseUnityPlugin
+    {
+        public const string pluginGuid = "gnomedeplume.againstthestorm.blockpause";
+        public const string pluginName = "ATS Block Pause";
+        public const string pluginVersion = "0.1";
+
+        public void Awake()
+        {
+            Logger.LogInfo("Disabling pausing...");
+
+            Harmony harmony = new Harmony(pluginGuid);
+
+            MethodInfo originalIsPauseBlocked = AccessTools.Method(typeof(EffectsService), "IsPauseBlocked");
+            MethodInfo isPauseBlockedPostPatch = AccessTools.Method(typeof(PausePatches), "IsPauseBlocked_PostPatch");
+
+            harmony.Patch(originalIsPauseBlocked, null, new HarmonyMethod(isPauseBlockedPostPatch));
+
+            Logger.LogInfo("Pausing disabled. Good luck!");
+        }
+    }
+}
---
+++ PausePatches.cs
@@ -0,0 +1,12 @@
+﻿using Eremite.Services;
+
+namespace ATSBlockPause
+{
+    class PausePatches
+    {
+        public static void IsPauseBlocked_PostPatch(EffectsService __instance, ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
